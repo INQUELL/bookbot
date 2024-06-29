29363c8ebef6823f1f5e69914009a994a1b88525
INQUELL <maxprediger@gmail.com>
2024-06-29 20:27:52 +0100

    Changed letterCount function to sort the result, exclude non-alphanumeric characters, and print a report on the .txt file

--- main.py
+++ main.py
@@ -18,11 +18,20 @@ def letterCount():
         letters_dict = {}
 
         for letter in file_lower:
-            if letter in letters_dict:
-                letters_dict[letter] += 1
-            else:
-                letters_dict[letter] = 1
-        return(letters_dict)
+            if letter.isalpha(): # Removes anything that isn't a letter
+                if letter in letters_dict:
+                    letters_dict[letter] += 1
+                else:
+                    letters_dict[letter] = 1
+        #return(letters_dict)
+        
+        # Remember, sometimes you gotta convert data between types to use them. 
+        # Here, we converted the data in the list into tuples so we can sort them.
+        letters_list = list(letters_dict.items())
+        # Lambda is a way to make a small anonymous function on the fly
+        sorted_letters = sorted(letters_list, reverse=True, key=lambda x: x[1])
+        print(sorted_letters)
+
 
 # def dictNum(dict):
 #     return dict.values()
@@ -31,17 +40,13 @@ def sortMe(dict):
     # unsortedDict = letterCount()
     # unsortedDict.sort(reverse=True, key=dictNum)
     # print(unsortedDict)
-    
-    
-
-
     pass
 
 
 def main():
     print("=== Begin report of books/frankenstein.txt ===")
     wordCount()
-
+    letterCount()
     print("=== End of report ===")
 
 main()
